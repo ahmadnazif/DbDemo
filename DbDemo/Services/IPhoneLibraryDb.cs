a36618e83253923b846e9eab2afeebd7be85605a
Nazif Abdullah <ahmadnazif@gmail.com>
2025-01-14 15:22:38 +0800

    Update SetAsync

--- DbDemo/Services/IPhoneLibraryDb.cs
+++ DbDemo/Services/IPhoneLibraryDb.cs
@@ -5,8 +5,6 @@ public interface IPhoneLibraryDb
     Task<bool> IsExistAsync(string msisdn);
     Task<Msisdn> GetAsync(string msisdn);
     Task<string> GetAsync(string msisdn, MsisdnField field);
-    Task<ResponseBase> SetAsync(string msisdn, string @operator, DateTime? updateTime = null);
-    //[Obsolete("Use the one in dedicated interface")] Task<long> CountRowAsync(int? index = null);
-    //Task<ResponseBase> InsertBatchAsync(List<PhoneNumber> list);
+    Task<ResponseBase> SetAsync(string msisdn, string @operator);
     Task<ResponseBase> DeleteAsync(string msisdn);    
 }
